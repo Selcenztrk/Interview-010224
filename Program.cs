908157806bed9afdc3ceaf4fa7116dfe8eeeed20
Selcen Öztürk <selcenztrk@gmail.com>
2024-02-01 18:55:57 +0300

    reformat code

--- Program.cs
+++ Program.cs
@@ -8,13 +8,13 @@ namespace Sestek3
         {
             int numerator;
             int denominator;
-           
-           public RationalNumber() { }
-           public RationalNumber(int numerator, int denominator)
+
+            public RationalNumber() { }
+            public RationalNumber(int numerator, int denominator)
             {
-             int value = GCD(numerator, denominator);    
-                this.numerator = numerator/value;
-                this.denominator = denominator/value;
+                int value = GCD(numerator, denominator);
+                this.numerator = numerator / value;
+                this.denominator = denominator / value;
 
             }
             static int GCD(int num1, int num2)
@@ -30,20 +30,21 @@ namespace Sestek3
 
                 return num1;
             }
-
-            public static RationalNumber Add(RationalNumber num1, RationalNumber num2) {
-               RationalNumber result = new RationalNumber();
-                result.numerator = (num1.numerator*num2.denominator) + (num2.numerator*num1.denominator);
-                result.denominator = num1.denominator * num2.denominator;
-                return result;
-            }
-
             public override string ToString()
             {
                 return this.numerator + "/" + this.denominator;
             }
 
-           public static RationalNumber Divide(RationalNumber num1, RationalNumber num2) {
+            public static RationalNumber Add(RationalNumber num1, RationalNumber num2)
+            {
+                RationalNumber result = new RationalNumber();
+                result.numerator = (num1.numerator * num2.denominator) + (num2.numerator * num1.denominator);
+                result.denominator = num1.denominator * num2.denominator;
+                return result;
+            }
+
+            public static RationalNumber Divide(RationalNumber num1, RationalNumber num2)
+            {
 
                 RationalNumber result = new RationalNumber();
                 result.numerator = num1.numerator * num2.denominator;
@@ -55,16 +56,17 @@ namespace Sestek3
                 result.denominator = result.denominator / value;
                 return result;
             }
-            public static RationalNumber Multiply(RationalNumber num1, RationalNumber num2) {
+            public static RationalNumber Multiply(RationalNumber num1, RationalNumber num2)
+            {
 
                 RationalNumber result = new RationalNumber();
-                result.numerator = num1.numerator*num2.numerator;
-                result.denominator = num1.denominator*num2.denominator;
+                result.numerator = num1.numerator * num2.numerator;
+                result.denominator = num1.denominator * num2.denominator;
 
                 int value = GCD(result.numerator, result.denominator);
 
-                result.numerator = result.numerator/value;
-                result.denominator = result.denominator/value;  
+                result.numerator = result.numerator / value;
+                result.denominator = result.denominator / value;
                 return result;
             }
             public static RationalNumber Substract(RationalNumber num1, RationalNumber num2)
@@ -78,23 +80,25 @@ namespace Sestek3
         }
         static void Main(string[] args)
         {
-           RationalNumber num1 = new RationalNumber(4,3);
-           RationalNumber num2 = new RationalNumber(6,2);
+            RationalNumber num1 = new RationalNumber(4, 3);
+            RationalNumber num2 = new RationalNumber(6, 2);
 
-           var num3 = RationalNumber.Substract(num1, num2);
+            var num3 = RationalNumber.Multiply(num1, num2);
 
 
-           Console.WriteLine("Result:" + num3);
+            // Console.WriteLine("Result:" + num3);
             Console.ReadLine();
 
-          // var addition = num1 + num2;
-          //  var num4 = num1 + 8
+           // RationalNumber addition = num1 + num2;
 
-      
+            Console.WriteLine(addition);
+            //  var num4 = num1 + 8
 
-            
 
-            
+
+
+
+
         }
     }
 }
